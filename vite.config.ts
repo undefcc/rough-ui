06dd4be411d236e11629e2e6b2e9ec60e6a8f3b2
ccbyte <slepox@126.com>
2022-07-16 16:58:12 +0800

    feature: markdown-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
+import Markdown from 'vite-plugin-md'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,5 @@ export default defineConfig({
       '@pack': path.resolve(__dirname, 'packages'),
     },
   },
-  plugins: [vue()],
+  plugins: [vue({ include: [/\.vue$/, /\.md$/] }), Markdown()],
 })
